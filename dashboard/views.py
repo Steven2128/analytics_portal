20120325a86fb5d6586fdc25e99754b05344ac82
Stephen Alarcón <slj212885@gmail.com>
2026-01-07 21:01:51 -0500

    feat: Dashboard View

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,43 @@
+# Django
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class DashboardView(View):
+    def get(self, request):
+        data = [
+            {
+                "fecha": "2026-01-07",
+                "cliente": "Helena Gomez",
+                "metrica": "Ventas",
+                "valor": 120
+            },
+            {
+                "fecha": "2024-02-21",
+                "cliente": "Pablo Alvarez",
+                "metrica": "Clientes",
+                "valor": 90
+            },
+            {
+                "fecha": "2026-02-20",
+                "cliente": "Sandra Muñoz",
+                "metrica": "Ingresos",
+                "valor": 437
+            },
+            {
+                "fecha": "2021-12-12",
+                "cliente": "Edgar Morales",
+                "metrica": "Pedidos",
+                "valor": 83782
+            },
+            {
+                "fecha": "203-06-01",
+                "cliente": "Daniel Ballesteros",
+                "metrica": "Costos",
+                "valor": 2832
+            }
+        ]
+
+        return render(request, 'dashboard/dashboard.html', {
+            "titulo": "Dashboard",
+            "data": data
+        })
